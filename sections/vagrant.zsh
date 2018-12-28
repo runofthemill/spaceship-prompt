134bdfb927b2f878dbd896f5a6867d6e5d664928
jeremy miller <jeremyemiller@gmail.com>
2018-12-27 22:58:02 -0800

    revert to original method to get status

--- sections/vagrant.zsh
+++ sections/vagrant.zsh
@@ -43,63 +43,29 @@ spaceship_vagrant() {
 
   # Use quotes around unassigned local variables to prevent
   # getting replaced by global aliases
-  # http://zsh.sourc3eforge.net/Doc/Release/Shell-Grammar.html#Aliasing
+  # http://zsh.sourceforge.net/Doc/Release/Shell-Grammar.html#Aliasing
   local 'vagrant_status'
 
-#   vagrant_prompt_info() {
-#   test -d .vagrant && test -f Vagrantfile
-#   if [[ "$?" == "0" ]]; then
-#     local 'statuses'
-#     statuses=$(vagrant status 2> /dev/null | grep -P "\w+\s+[\w\s]+\s\(\w+\)")
-#     statuses=("${(f)statuses}")
-#     for vm_details in $statuses; do
-#       vm_state=$(echo $vm_details | grep -o -E "saved|poweroff|not created|running|aborted")
-#       if [[ "$vm_state" == "running" ]]; then
-#         printf '%s' $SPACESHIP_VAGRANT_RUNNING
-#       elif [[ "$vm_state" == "saved" ]]; then
-#         printf '%s' $SPACESHIP_VAGRANT_SUSPENDED
-#       elif [[ "$vm_state" == "not created" ]]; then
-#         printf '%s' $SPACESHIP_VAGRANT_NOT_CREATED
-#       elif [[ "$vm_state" == "poweroff" ]]; then
-#         printf '%s' $SPACESHIP_VAGRANT_POWEROFF
-#       elif [[ "$vm_state" == "aborted" ]]; then
-#         printf '%s' $SPACESHIP_VAGRANT_ABORTED
-#       fi
-#     done
-#   fi
-# }
-
-vagrant_prompt_info() {
+  vagrant_prompt_info() {
   test -d .vagrant && test -f Vagrantfile
   if [[ "$?" == "0" ]]; then
-  if spaceship::exists jq; then
-    vagrant_state=$(cat "$HOME/.vagrant.d/data/machine-index/index" |
-jq -r --arg dir "$PWD" '.machines[] | select(.vagrantfile_path == $dir).state')
-  else
-    vagrant_state=$(cat ${HOME}/.vagrant.d/data/machine-index/index | python -c 'import sys, os, json;
-json_file = json.load(sys.stdin)["machines"]
-for box in json_file:
-  if (json_file[box]["vagrantfile_path"] == os.getcwd()):
-    print (json_file[box]["state"])
-    break;
-')
-  fi
-
-  if ! $SPACESHIP_VAGRANT_DISPLAY_SHORT; then
-  printf $vagrant_state
-  else
-    if [[ "$vagrant_state" == "running" ]]; then
-      printf '%s' $SPACESHIP_VAGRANT_RUNNING
-    elif [[ "$vagrant_state" == "saved" ]]; then
-      printf '%s' $SPACESHIP_VAGRANT_SUSPENDED
-    elif [[ "$vagrant_state" == "not created" ]]; then
-      printf '%s' $SPACESHIP_VAGRANT_NOT_CREATED
-    elif [[ "$vagrant_state" == "poweroff" ]]; then
-      printf '%s' $SPACESHIP_VAGRANT_POWEROFF
-    elif [[ "$vagrant_state" == "aborted" ]]; then
-      printf '%s' $SPACESHIP_VAGRANT_ABORTED
-    fi
-  fi
+    local 'statuses'
+    statuses=$(vagrant status 2> /dev/null | grep -P "\w+\s+[\w\s]+\s\(\w+\)")
+    statuses=("${(f)statuses}")
+    for vm_details in $statuses; do
+      vm_state=$(echo $vm_details | grep -o -E "saved|poweroff|not created|running|aborted")
+      if [[ "$vm_state" == "running" ]]; then
+        printf '%s' $SPACESHIP_VAGRANT_RUNNING
+      elif [[ "$vm_state" == "saved" ]]; then
+        printf '%s' $SPACESHIP_VAGRANT_SUSPENDED
+      elif [[ "$vm_state" == "not created" ]]; then
+        printf '%s' $SPACESHIP_VAGRANT_NOT_CREATED
+      elif [[ "$vm_state" == "poweroff" ]]; then
+        printf '%s' $SPACESHIP_VAGRANT_POWEROFF
+      elif [[ "$vm_state" == "aborted" ]]; then
+        printf '%s' $SPACESHIP_VAGRANT_ABORTED
+      fi
+    done
   fi
 }
 
